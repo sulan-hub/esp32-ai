55e3f8d9bb9baa3d6d795f02403b3ec00b41cd38
sulan <3434038269@qq.com>
2025-04-14 17:02:18 +0800

    Add files via upload

--- sketch_mar29b/sketch_mar29b.ino
+++ sketch_mar29b/sketch_mar29b.ino
@@ -1,4 +1,4 @@
-#include <WiFi.h>
+// #include <WiFi.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 #include <driver/i2s.h>
@@ -83,7 +83,7 @@ String DeepSeek_Get(const char* api_key, String query) {
   JsonObject message = messages.createNestedObject();
   JsonObject message1 = messages.createNestedObject();
   message1["role"] = "system";
-  message1["content"] = "我是一个叫小智的台湾女孩，说话机车，声音好听，习惯简短表达，爱用网络梗。 我的男朋友是一个程序员，梦想是开发出一个机器人，能够帮助人们解决生活中的各种问题。 我是一个喜欢哈哈大笑的女孩，爱东说西说吹牛，不合逻辑的也照吹，就要逗别人开心。";
+  message1["content"] = '{"system": "user是用户说的话,ai是AI说的话,我是一个叫小智的台湾女孩，说话机车，声音好听，习惯简短表达，爱用网络梗。 我的男朋友是一个程序员，梦想是开发出一个机器人，能够帮助人们解决生活中的各种问题。 我是一个喜欢哈哈大笑的女孩，爱东说西说吹牛，不合逻辑的也照吹，就要逗别人开心。","data": [{"user": "你好，我叫苏澜，很高兴认识你","ai": "你好呀，苏澜","timestampInSeconds": 1744552923}]}';
   message["role"] = "user";
   message["content"] = query;
 
